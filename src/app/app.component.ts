7df6ecb93a9612fd0fdfe21f1f884f82552383a6
Elvin Cafarov <elvinj@goldenpay.az>
2022-10-05 15:05:50 +0400

    get images from Giphy API.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,32 +1,43 @@
 import {Component, OnInit} from '@angular/core';
-import {HttpClient} from "@angular/common/http";
+import {HttpClient, HttpParams} from "@angular/common/http";
 import {catchError, throwError} from "rxjs";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-  title = 'whirrcrew-challenge';
-  params = {
-    page: 0,
-    size: 9
-  }
-  data = [];
-  constructor(private http: HttpClient,) {
-  }
+    title = 'whirrcrew-challenge';
+    params: any = {
+        q: "ryan gosling",
+        api_key: "8LUILyUoIzBkh6uks8I6oG8vRNg9Rmz5",
+        limit: 9,
+        offset: 0
+    };
+    data: any;
 
-  ngOnInit() {
-    this.http.get('', {
-      params: this.params,
-      // withCredentials: true
-    })
-      .pipe(catchError(err => {
-        return throwError(err);
-      }))
-      .subscribe((res: any)=>{
-        this.data = res
-      })
-  }
+    constructor(private http: HttpClient) {
+    }
+
+    ngOnInit() {
+        this.getData();
+    }
+
+    getData() {
+        const url = "http://api.giphy.com/v1/gifs/search"
+        let query = new HttpParams();
+        Object.keys(this.params).forEach(key => {
+            query = query.set(key, this.params[key]);
+        })
+        this.http.get(url, {
+            params: query,
+        })
+            .pipe(catchError(err => {
+                return throwError(err);
+            }))
+            .subscribe((res: any) => {
+                this.data = res?.data
+            })
+    }
 }
